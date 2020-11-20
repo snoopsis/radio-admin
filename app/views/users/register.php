6b2a9eaf3608a59cc8540b1ba7dae2c7cabddf7e
snoopsis <tarantula.lan@gmail.com>
2020-11-20 20:44:57 -0300

    update

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -9,13 +9,13 @@
 
 <div class="form-group">
 <label for="name">Nome: <sup>*</sup></label>
-<input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
+<input type="text" name="name" id="nome" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
 <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
 </div>
 
 <div class="form-group">
 <label for="email">Email: <sup>*</sup></label>
-<input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+<input type="email" name="email" id="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
 <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
 </div>
 
@@ -34,13 +34,13 @@
 
 <div class="form-group">
 <label for="confirm_password">Confirme Senha: <sup>*</sup></label>
-<input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
+<input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>" id="enviar">
 <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
 </div>
 
 <div class="row">
 <div class="col">
-<input type="submit" class="btn btn-success btn-block" value="Registro">
+<input type="submit" class="btn btn-success btn-block" id="enviado" value="Registro">
 </div>
 <div class="col">
 <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Ja tem uma conta? Entrar</a>
